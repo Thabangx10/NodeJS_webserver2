e813211fb8175d2ff91d0dbeed90d9ce4ae03e75
Thabangx10 <Thabangx12@gmail.com>
2023-08-02 16:00:05 +0200

    first_attempt

---
+++ config/index.js
@@ -0,0 +1,15 @@
+// We are going to create a connection to a database of our choice
+
+require('dotenv').config()
+const {createPool} = require('mysql')
+let connection = createPool({
+    host: process.env.dbHost,
+    user: process.env.dbUser,
+    password: process.env.dbPwd,
+    port: process.env.dbPort,
+    database: process.env.dbName,
+    multipleStatements: true,
+    connectionLimit: 30
+})
+
+module.exports = connection
